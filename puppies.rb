552e55dcc4c1a9f0542ad702d5cceca7cdd61b4e
Sarahxdoughty <sarahxdoughty@gmail.com>
2018-07-24 18:56:26 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -8,11 +8,27 @@ def no_puppies(num)
 end
 
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
-
+def less_puppies(num)
+  if num < 3
+    return "good job"
+  elsif num > 3
+    return "get back on your animal restriction"
+  else
+    return "slow down"
+  end
+end
 
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
-
+def less_puppies(num, numAllowed)
+  if num < numAllowed/2
+    return "good job"
+  elsif num > numAllowed
+    return "get back on your animal restriction"
+  else
+    return "slow down"
+  end
+end
 
 
 # BONUS
